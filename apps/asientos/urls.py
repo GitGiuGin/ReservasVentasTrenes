63a1f1b44b64e58208263f306a458cac786572a2
GitGiuGin <jmacross_1995@hotmail.com>
2024-09-16 14:57:06 -0400

    Semana de examenes - ajustes de examenes

--- apps/asientos/urls.py
+++ apps/asientos/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('seleccionaAsiento', reservasAsiento, name='seleccion_asiento'),
+    path('reserve/<int:id>', formSelectAsiento, name='seleccion_asiento'),
 ]
--- apps/asientos/views.py
+++ apps/asientos/views.py
@@ -1,15 +1,51 @@
 from django.shortcuts import render
+from apps.trenes.models import Tren
+from apps.rutas.models import Ruta
+from apps.reservas.models import Reserva
+from apps.asientos.models import Asiento
+from datetime import datetime
 
 # Create your views here.
-def liberarAsiento():
-    pass
+def obtenerFechaActual(request):
+    fecha_actual = datetime.now().strftime("%d/%m/%Y")
+    return fecha_actual
 
-def obtenerEstado():
-    pass
+def liberarAsiento(asiento_id):
+    try:
+        asiento = Asiento.objects.get(id=asiento_id)
+        asiento.estado = False
+        asiento.reserva = None
+        asiento.save()
+        return True
+    except Asiento.DoesNotExist:
+        return False
 
-def reservasAsiento(request):
-    # Aquí colocas la lógica para manejar la selección de asientos
-    return render(request, 'asientos/seleccion_asiento.html')
+def obtenerEstado(asiento_id):
+    try:
+        asiento = Asiento.objects.get(id=asiento_id)
+        return "Reservado" if asiento.estado else "Disponible"
+    except Asiento.DoesNotExist:
+        return "Asiento no encontrado"
 
-def verificarDisponibilidad():
-    pass
+def formSelectAsiento(request, id):
+    fecha_actual = obtenerFechaActual(request)
+    ruta = Ruta.objects.get(id=id)
+    asientos = Asiento.objects.all()
+    #eserva = Reserva.objects.all()
+    
+    data = {
+        "ruta": ruta,
+        "fecha_actual": fecha_actual,
+        "asiento": asientos,
+        #"reserva": reserva,
+    }
+    
+    return render(request, 'asientos/seleccion_asiento.html', data)
+
+def verificarDisponibilidad(ruta_id):
+    asientos_disponibles = Asiento.objects.filter(ruta_id=ruta_id, estado=False).count()
+    return asientos_disponibles > 0
+
+def obtenerTrenes(request):
+    trenes_activos = Tren.objects.filter(estado=True)
+    
